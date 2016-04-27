5ba38d908ee2927a6b493a581bc6815fe958933c
Guillermo <gui@mit.edu>
2016-04-27 19:15:30 -0400

    codemirror works

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+import './main.css'
+
+import CodeMirror from 'codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/scheme/scheme'
+import 'codemirror/theme/monokai.css'
+import './theme.css'
+
+// import 'codemirror/codemirror.css'
+
+CodeMirror(document.querySelector('.codemirror-container'), {
+  value: "(+ 1 1)\n",
+  mode:  "scheme",
+  theme: 'monokai'
+})
