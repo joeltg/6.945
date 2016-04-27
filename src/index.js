943eb3ff98f2969ad564d7406ca2d1a3361c96e2
Guillermo <gui@mit.edu>
2016-04-27 19:20:01 -0400

    made codemirror better

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import './main.css'
 
 import CodeMirror from 'codemirror'
 import 'codemirror/lib/codemirror.css'
-import 'codemirror/mode/scheme/scheme'
 import 'codemirror/theme/monokai.css'
 import './theme.css'
 
-// import 'codemirror/codemirror.css'
+import 'codemirror/mode/scheme/scheme'
 
-CodeMirror(document.querySelector('.codemirror-container'), {
-  value: "(+ 1 1)\n",
-  mode:  "scheme",
-  theme: 'monokai'
-})
+import 'codemirror/keymap/sublime'
+
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/addon/edit/matchbrackets'
+
+import 'codemirror/addon/comment/comment'
+
+
+const cmoptions = {
+    value: "(+ 1 1)\n",
+    mode:  "scheme",
+    theme: 'monokai',
+    indentUnit: 4,
+    indentWithTabs: false,
+    keyMap: 'sublime',
+    autoCloseBrackets: true,
+    matchBrackets: true,
+}
+
+
+CodeMirror(document.querySelector('.codemirror-container'), cmoptions)
