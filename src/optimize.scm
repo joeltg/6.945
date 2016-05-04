7651d15a96ee1e15fb7ccfcaa8a7621e1104263f
Joel Gustafson <joelg@mit.edu>
2016-05-04 01:27:05 -0400

    beginning to look like something, maybe?

--- src/optimize.scm
+++ src/optimize.scm
@@ -1,41 +1,45 @@
-(load "numeric-optimization.scm")
+(load "generic-operators")
 
-(define (delta a b)
-  (square (- a b)))
 
-(define (renderable-diff r1 r2)
+(define delta (make-generic-operator 2 'delta delta:symbolic))
+
+(defhandler delta delta:numeric number? number?)
+
+(define delta:numeric -)
+
+(define (delta:symbolic a b)
+  (if (eq? a b) 0 1))
+
+(define (map-assq f alist . alists)
+  (let iter ((alist alist) (alists alists) (result '()))
+    (if (null? alist)
+      result
+      (let* ((key (caar alist))
+             (collect (map (lambda (l) (assq key l))) alists))
+          (if (any not collect)
+            (iter (cdr alist) alists result)
+            (let* ((values (map cadar collect))
+                   (new-result (cons (list key (apply f values)) result)))
+              (iter (cdr alist) alists new-result)))))))
+
+(define (sse r1 r2)
   (let iter ((r1 r1) (r2 r2) (sum 0))
     (if (null? r1)
       sum
-      (let* ((key (car r1)) (val1 (cadar r1)) (val2 (assq r2)))
+      (let ((key (car r1)) (val1 (cadar r1)) (val2 (assq r2)))
         (if val2
           (iter (cdr r1) r2 (+ sum (delta val1 val2)))
           (iter (cdr r1) r2 sum))))))
 
-(define (optimize data->renderables
-                  current-data
-                  current-renderable
-                  desired-renderable)
+
+(define (optimize:numeric data->renderables
+                          current-data
+                          current-renderable
+                          desired-renderable)
   (let* ((data-keys (map car current-data))
          (data-vals (map cdr current-data))
          (f (lambda (vals)
-              (renderable-diff
+              (sse
                 current-renderables
                 (data->renderables (map list data-keys vals))))))
     (multidimensional-minimize f data-vals)))
-
-(define (multidimensional-minimize f parameters)
-  (let ((f (lambda (x) (f (vector->list x)))))
-    (let ((result
-	   (nelder-mead f
-			(list->vector parameters)
-			nelder-start-step
-			nelder-epsilon
-			nelder-maxiter)))
-      (if (eq? 'ok (car result))
-	  (vector->list (caadr result))
-	  (error "Minimizer did not converge")))))
-
-(define nelder-start-step .01)
-(define nelder-epsilon 1.0e-10)
-(define nelder-maxiter 1000)
